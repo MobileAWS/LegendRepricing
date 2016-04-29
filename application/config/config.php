6a4193e8365c8efb8fdfdee314a52e703ee7c190
Imran Arshad <imran@appifytech.com>
2016-04-29 18:50:29 +0500

    fixed session cookie path

--- application/config/config.php
+++ application/config/config.php
@@ -277,7 +277,7 @@ $config['sess_time_to_update']	= 300;
 */
 $config['cookie_prefix']	= "";
 $config['cookie_domain']	= "";
-$config['cookie_path']		= "/";
+$config['cookie_path']		= "/maws_dev";
 $config['cookie_secure']	= FALSE;
 
 /*
