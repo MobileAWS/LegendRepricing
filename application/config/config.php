3e3721483d090f04aa31722e5b3af64eaced6338
imran-maws <imran@mobileaws.com>
2016-04-29 18:34:42 +0500

    fixed config

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-error_reporting(E_ALL);
+error_reporting(E_ALL-E_NOTICE);
 
 /*
 |--------------------------------------------------------------------------
