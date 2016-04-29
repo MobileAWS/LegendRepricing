b5d04a40ee3690de600f3133f425b2931647ba02
imran-maws <imran@mobileaws.com>
2016-04-29 18:30:48 +0500

    update db credentials

--- application/config/database.php
+++ application/config/database.php
@@ -51,11 +51,9 @@ $active_record = TRUE;
 
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-//$db['default']['password'] = 'harekrishnaharekrishna1594';
-//$db['default']['database'] = 'amazon';
-$db['default']['password'] = 'mysql';
-$db['default']['database'] = 'legendpricing';
+$db['default']['username'] = 'devlp';
+$db['default']['password'] = 'kahnaika7y65';
+$db['default']['database'] = 'dev_legendrepricing';
 
 
 $db['default']['dbdriver'] = 'mysql';
