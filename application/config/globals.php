9d87c3061bc59996214d3f6a565fb4e2ee0090dd
Imran Arshad <imran@appifytech.com>
2016-04-30 03:04:28 +0500

    added debug to globals.php

--- application/config/globals.php
+++ application/config/globals.php
@@ -7,4 +7,10 @@ $config['currency']= '';
 $config['amazon_url']= 'http://www.amazon.com/gp/product/';
 $config['seller_url']= 'http://www.amazon.com/gp/aag/main?ie=UTF8&seller=';
 
+function debug($v){
+    print_r('<pre>');
+    print_r($v);
+    print_r('</pre>');
+}
+
 ?>
